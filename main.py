cd2a99bb1d25f657c0b4b73c73c806345fcaf5ba
emmab74330 <emmab74330@gmail.com>
2024-10-29 16:02:46 +0100

    Utilisation de pylint et modification du code afin de respecter 100% des standards pylint.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+"""Module principal."""
+
 def addition(a, b):
+    """Addition : Retourne la somme de a et b."""
     return a + b
 
-
 def division(a, b):
+    """Division : Retourne la division de a par b."""
     return a / b
-
--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
-# test_main.py
+"""Tests pour le module principal : main.py."""
+
 import pytest
 from main import addition
 
@@ -7,5 +8,6 @@ from main import addition
     (2, 3, 5)
 ])
 def test_addition(a, b, result):
-     a, b = 2,3
-     assert addition(a, b) == result
+    """Test de la fonction d'addition avec les valeurs paramétrées."""
+    a, b = 2,3
+    assert addition(a, b) == result
