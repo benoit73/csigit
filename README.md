fc6aabc1fa4a4a58d3aa409b97123ea8e315bbfd
kiliannnnn <coudurier.kilian@gmail.com>
2024-10-29 15:46:41 +0100

    added docstring to functions and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# CSIGIT
+## Functions
+- addition(a, b) Return the result of the addition of the 2 given parameters
+- division(a, b) Return the result of the division of the 2 given parameters
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def addition(a, b):
+    """Return the result of the addition of the 2 given parameters"""
     return a + b
 
 
 def division(a, b):
+    """Return the result of the division of the 2 given parameters"""
     return a / b
